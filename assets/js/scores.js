7181afeccaf9c291e0783054d6c0915c2a41f43b
Jag Johal <jagjohal@Jags-MacBook-Air.local>
2023-09-01 19:23:13 +0100

    update to questions js file

---
+++ assets/js/scores.js
@@ -0,0 +1,21 @@
+function printHighScores() {
+//TODO
+
+}
+
+
+function clearHighScores(){
+//TODO
+
+}
+
+let clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearHighScores);
+// or
+
+// document.getElementById("clear").onClick = clearHighScores;
+
+
+printHighScores();
+
+
